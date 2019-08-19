e39b7bc16de0c280a7946d575aa3fb94dcfeee1c
hsinlei <hsinlei.w@gmail.com>
2019-08-19 15:45:15 -0700

    integrate search.html into React

--- src/components/SearchFilters.js
+++ src/components/SearchFilters.js
@@ -10,7 +10,13 @@ import Flex, { FlexChild } from '../styles/Flex';
 const SearchFilters = () => (
 	<Flex responsive style={{ padding: '1rem' }}>
 		<FlexChild flex={2}>
-			
+			<form method = "get" title = "Search Form" action="https://cse.google.com/cse/publicurl">
+			 <div>
+			    <input type="text" id="q" name="q" title="Search this site" alt="Search Text" maxlength="256" />
+			    <input type="hidden" id="cx" name="cx" value="008518891864161743594:5lv7n5p4qru" />
+			   <input type="image" id="searchSubmit" name="submit" src="https://www.flaticon.com/free-icon/active-search-symbol_34148" alt="Search" title="Submit Search Query" />
+			 </div>
+			</form>
 		</FlexChild>
 		<FlexChild flex={1}>
 			<SingleDropdownList
