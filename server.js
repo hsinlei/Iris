19c3b1d594d87236e357cf01f4fee7bd77205345
hsinlei <hsinlei.w@gmail.com>
2019-08-20 11:12:24 -0700

    server update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use(function(request, response, next) {
-	response.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	response.header("Access-Control-Allow-Origin", "*"); // TODO: update to match the domain you will make the request from
   	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   	next();
 });
