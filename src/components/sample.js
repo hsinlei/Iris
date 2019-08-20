5e729fdd4c9ecc14ed97f1575a4947d7920bbdda
Marley <marleyxiong0@gmail.com>
2019-08-20 11:20:24 -0700

    mock data

---
+++ src/components/sample.js
@@ -0,0 +1,374 @@
+const sample = {
+ "kind": "customsearch#search",
+ "url": {
+  "type": "application/json",
+  "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+ },
+ "queries": {
+  "request": [
+   {
+    "title": "Google Custom Search - brain",
+    "totalResults": "66400000",
+    "searchTerms": "brain",
+    "count": 10,
+    "startIndex": 1,
+    "inputEncoding": "utf8",
+    "outputEncoding": "utf8",
+    "safe": "off",
+    "cx": "008518891864161743594:5lv7n5p4qru"
+   }
+  ],
+  "nextPage": [
+   {
+    "title": "Google Custom Search - brain",
+    "totalResults": "66400000",
+    "searchTerms": "brain",
+    "count": 10,
+    "startIndex": 11,
+    "inputEncoding": "utf8",
+    "outputEncoding": "utf8",
+    "safe": "off",
+    "cx": "008518891864161743594:5lv7n5p4qru"
+   }
+  ]
+ },
+ "context": {
+  "title": "Iris"
+ },
+ "searchInformation": {
+  "searchTime": 0.407905,
+  "formattedSearchTime": "0.41",
+  "totalResults": "66400000",
+  "formattedTotalResults": "66,400,000"
+ },
+ "items": [
+  {
+   "kind": "customsearch#result",
+   "title": "Essential fatty acids and human brain.",
+   "htmlTitle": "Essential fatty acids and human \u003cb\u003ebrain\u003c/b\u003e.",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/20329590",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "We've learned in recent years that fatty acids are among the most crucial \nmolecules that determine your brain's integrity and ability to perform. Essential \nfatty ...",
+   "htmlSnippet": "We&#39;ve learned in recent years that fatty acids are among the most crucial \u003cbr\u003e\nmolecules that determine your \u003cb\u003ebrain&#39;s\u003c/b\u003e integrity and ability to perform. Essential \u003cbr\u003e\nfatty&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/20329590",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/20329590",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Essential fatty acids and human brain.",
+      "citation_authors": "Chang CY; Ke DS; Chen JY",
+      "citation_date": "12/2009",
+      "citation_publisher": "Acta Neurol Taiwan",
+      "citation_journal_title": "Acta neurologica Taiwanica",
+      "citation_pmid": "20329590",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/20329590",
+      "citation_volume": "18",
+      "citation_issue": "4",
+      "citation_firstpage": "231",
+      "citation_issn": "1028-768X"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Brain foods: the effects of nutrients on brain function",
+   "htmlTitle": "\u003cb\u003eBrain\u003c/b\u003e foods: the effects of nutrients on \u003cb\u003ebrain\u003c/b\u003e function",
+   "link": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2805706/",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "The brain consumes an immense amount of energy relative to the rest of the \nbody. Thus, the mechanisms that are involved in the transfer of energy from foods \nto ...",
+   "htmlSnippet": "The \u003cb\u003ebrain\u003c/b\u003e consumes an immense amount of energy relative to the rest of the \u003cbr\u003e\nbody. Thus, the mechanisms that are involved in the transfer of energy from foods \u003cbr\u003e\nto&nbsp;...",
+   "cacheId": "zg5qAni8VCMJ",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2805706/",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2805706/",
+   "pagemap": {
+    "cse_thumbnail": [
+     {
+      "width": "310",
+      "height": "163",
+      "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpxSTTHwJ6Gf0XYLEQeRsiluGC65YA9D4zg5_hMU2Gy5u3kQKAuBIahZQ"
+     }
+    ],
+    "metatags": [
+     {
+      "ncbi_db": "pmc",
+      "ncbi_pdid": "article",
+      "ncbi_domain": "nihpa",
+      "ncbi_report": "record",
+      "ncbi_type": "fulltext",
+      "ncbi_pcid": "/articles/PMC2805706/?report=reader&",
+      "ncbi_app": "pmc",
+      "apple-mobile-web-app-capable": "no",
+      "viewport": "initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,width=device-width",
+      "format-detection": "telephone=no",
+      "citation_journal_title": "Nature reviews. Neuroscience",
+      "citation_title": "Brain foods: the effects of nutrients on brain function",
+      "citation_authors": "Fernando Gómez-Pinilla",
+      "citation_date": "July 2008",
+      "citation_issue": "7",
+      "citation_volume": "9",
+      "citation_firstpage": "568",
+      "citation_doi": "10.1038/nrn2421",
+      "citation_abstract_html_url": "/pmc/articles/PMC2805706/?report=abstract",
+      "citation_pmid": "18568016",
+      "dc.title": "Brain foods: the effects of nutrients on brain function",
+      "dc.type": "Text",
+      "dc.publisher": "NIH Public Access",
+      "dc.contributor": "Fernando Gómez-Pinilla",
+      "dc.date": "2008 Jul",
+      "dc.identifier": "10.1038/nrn2421",
+      "dc.language": "en",
+      "og:title": "Brain foods: the effects of nutrients on brain function",
+      "og:type": "article",
+      "og:description": "It has long been suspected that the relative abundance of specific nutrients can affect cognitive processes and emotions. Newly described influences of dietary factors on neuronal function and synaptic plasticity have revealed some of the vital mechanisms ...",
+      "og:url": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2805706/",
+      "og:site_name": "PubMed Central (PMC)",
+      "og:image": "https://www.ncbi.nlm.nih.gov/corehtml/pmc/pmcgifs/pmc-logo-share.png",
+      "twitter:card": "summary",
+      "twitter:site": "@ncbi",
+      "citationexporter": "backend:'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc/'",
+      "pmcaccid": "PMC2805706",
+      "ncbi_phid": "CE8B910FD5A4C791000000000524031D.m_4",
+      "referrer": "origin-when-cross-origin"
+     }
+    ],
+    "cse_image": [
+     {
+      "src": "https://www.ncbi.nlm.nih.gov/corehtml/pmc/pmcgifs/pmc-logo-share.png"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Brain-derived neurotrophic factor produces antidepressant effects in ...",
+   "htmlTitle": "\u003cb\u003eBrain\u003c/b\u003e-derived neurotrophic factor produces antidepressant effects in ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/11943826",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "J Neurosci. 2002 Apr 15;22(8):3251-61. Brain-derived neurotrophic factor \nproduces antidepressant effects in behavioral models of depression. Shirayama \nY(1) ...",
+   "htmlSnippet": "J Neurosci. 2002 Apr 15;22(8):3251-61. \u003cb\u003eBrain\u003c/b\u003e-derived neurotrophic factor \u003cbr\u003e\nproduces antidepressant effects in behavioral models of depression. Shirayama \u003cbr\u003e\nY(1)&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/11943826",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/11943826",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Brain-derived neurotrophic factor produces antidepressant effects in behavioral models of depression.",
+      "citation_authors": "Shirayama Y; Chen AC; Nakagawa S; Russell DS; Duman RS",
+      "citation_date": "4/15/2002",
+      "citation_publisher": "J Neurosci",
+      "citation_journal_title": "The Journal of neuroscience : the official journal of the Society for Neuroscience",
+      "citation_pmid": "11943826",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/11943826",
+      "citation_volume": "22",
+      "citation_issue": "8",
+      "citation_firstpage": "3251",
+      "citation_doi": "20026292",
+      "citation_issn": "1529-2401"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Physical activity increases mRNA for brain-derived neurotrophic ...",
+   "htmlTitle": "Physical activity increases mRNA for \u003cb\u003ebrain\u003c/b\u003e-derived neurotrophic ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/8836544",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Brain Res. 1996 Jul 8;726(1-2):49-56. Physical activity increases mRNA for brain\n-derived neurotrophic factor and nerve growth factor in rat brain. Neeper SA(1) ...",
+   "htmlSnippet": "\u003cb\u003eBrain\u003c/b\u003e Res. 1996 Jul 8;726(1-2):49-56. Physical activity increases mRNA for \u003cb\u003ebrain\u003c/b\u003e\u003cbr\u003e\n-derived neurotrophic factor and nerve growth factor in rat \u003cb\u003ebrain\u003c/b\u003e. Neeper SA(1)&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/8836544",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/8836544",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Physical activity increases mRNA for brain-derived neurotrophic factor and nerve growth factor in rat brain.",
+      "citation_authors": "Neeper SA; Gómez-Pinilla F; Choi J; Cotman CW",
+      "citation_date": "7/8/1996",
+      "citation_publisher": "Brain Res",
+      "citation_journal_title": "Brain research",
+      "citation_pmid": "8836544",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/8836544",
+      "citation_volume": "726",
+      "citation_issue": "1-2",
+      "citation_firstpage": "49",
+      "citation_issn": "0006-8993"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Inflammation and white matter degeneration persist for years after a ...",
+   "htmlTitle": "Inflammation and white matter degeneration persist for years after a ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/23365092",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Brain. 2013 Jan;136(Pt 1):28-42. doi: 10.1093/brain/aws322. Inflammation and \nwhite matter degeneration persist for years after a single traumatic brain injury.",
+   "htmlSnippet": "\u003cb\u003eBrain\u003c/b\u003e. 2013 Jan;136(Pt 1):28-42. doi: 10.1093/\u003cb\u003ebrain\u003c/b\u003e/aws322. Inflammation and \u003cbr\u003e\nwhite matter degeneration persist for years after a single traumatic \u003cb\u003ebrain\u003c/b\u003e injury.",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/23365092",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/23365092",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Inflammation and white matter degeneration persist for years after a single traumatic brain injury.",
+      "citation_authors": "Johnson VE; Stewart JE; Begbie FD; Trojanowski JQ; Smith DH; Stewart W",
+      "citation_date": "1/2013",
+      "citation_publisher": "Brain",
+      "citation_journal_title": "Brain : a journal of neurology",
+      "citation_pmid": "23365092",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/23365092",
+      "citation_volume": "136",
+      "citation_issue": "Pt 1",
+      "citation_firstpage": "28",
+      "citation_doi": "10.1093/brain/aws322",
+      "citation_issn": "1460-2156"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Sleep-wake disturbances 6 months after traumatic brain injury: a ...",
+   "htmlTitle": "Sleep-wake disturbances 6 months after traumatic \u003cb\u003ebrain\u003c/b\u003e injury: a ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/17584779",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Brain. 2007 Jul;130(Pt 7):1873-83. Sleep-wake disturbances 6 months after \ntraumatic brain injury: a prospective study. Baumann CR(1), Werth E, Stocker R, ...",
+   "htmlSnippet": "\u003cb\u003eBrain\u003c/b\u003e. 2007 Jul;130(Pt 7):1873-83. Sleep-wake disturbances 6 months after \u003cbr\u003e\ntraumatic \u003cb\u003ebrain\u003c/b\u003e injury: a prospective study. Baumann CR(1), Werth E, Stocker R,&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/17584779",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/17584779",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Sleep-wake disturbances 6 months after traumatic brain injury: a prospective study.",
+      "citation_authors": "Baumann CR; Werth E; Stocker R; Ludwig S; Bassetti CL",
+      "citation_date": "7/2007",
+      "citation_publisher": "Brain",
+      "citation_journal_title": "Brain : a journal of neurology",
+      "citation_pmid": "17584779",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/17584779",
+      "citation_volume": "130",
+      "citation_issue": "Pt 7",
+      "citation_firstpage": "1873",
+      "citation_doi": "10.1093/brain/awm109",
+      "citation_issn": "1460-2156"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Brain-derived neurotrophic factor and antidepressant drugs have ...",
+   "htmlTitle": "\u003cb\u003eBrain\u003c/b\u003e-derived neurotrophic factor and antidepressant drugs have ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/15689544",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Brain-derived neurotrophic factor and antidepressant drugs have different but \ncoordinated effects on neuronal turnover, proliferation, and survival in the adult ...",
+   "htmlSnippet": "\u003cb\u003eBrain\u003c/b\u003e-derived neurotrophic factor and antidepressant drugs have different but \u003cbr\u003e\ncoordinated effects on neuronal turnover, proliferation, and survival in the adult&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/15689544",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/15689544",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Brain-derived neurotrophic factor and antidepressant drugs have different but coordinated effects on neuronal turnover, proliferation, and survival in the adult dentate gyrus.",
+      "citation_authors": "Sairanen M; Lucas G; Ernfors P; Castrén M; Castrén E",
+      "citation_date": "2/2/2005",
+      "citation_publisher": "J Neurosci",
+      "citation_journal_title": "The Journal of neuroscience : the official journal of the Society for Neuroscience",
+      "citation_pmid": "15689544",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/15689544",
+      "citation_volume": "25",
+      "citation_issue": "5",
+      "citation_firstpage": "1089",
+      "citation_doi": "10.1523/JNEUROSCI.3741-04.2005",
+      "citation_issn": "1529-2401"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Brain-computer communication: unlocking the locked in.",
+   "htmlTitle": "\u003cb\u003eBrain\u003c/b\u003e-computer communication: unlocking the locked in.",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/11393301",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Psychol Bull. 2001 May;127(3):358-75. Brain-computer communication: \nunlocking the locked in. Kübler A(1), Kotchoubey B, Kaiser J, Wolpaw JR, \nBirbaumer N.",
+   "htmlSnippet": "Psychol Bull. 2001 May;127(3):358-75. \u003cb\u003eBrain\u003c/b\u003e-computer communication: \u003cbr\u003e\nunlocking the locked in. Kübler A(1), Kotchoubey B, Kaiser J, Wolpaw JR, \u003cbr\u003e\nBirbaumer N.",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/11393301",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/11393301",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Brain-computer communication: unlocking the locked in.",
+      "citation_authors": "Kübler A; Kotchoubey B; Kaiser J; Wolpaw JR; Birbaumer N",
+      "citation_date": "5/2001",
+      "citation_publisher": "Psychol Bull",
+      "citation_journal_title": "Psychological bulletin",
+      "citation_pmid": "11393301",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/11393301",
+      "citation_volume": "127",
+      "citation_issue": "3",
+      "citation_firstpage": "358",
+      "citation_issn": "0033-2909"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Emotion, motivation, and the brain: reflex foundations in animal and ...",
+   "htmlTitle": "Emotion, motivation, and the \u003cb\u003ebrain\u003c/b\u003e: reflex foundations in animal and ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/17015072",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "This review will focus on a motivational circuit in the brain, centered on the \namygdala, that underlies human emotion. This neural circuitry of appetitive/\napproach ...",
+   "htmlSnippet": "This review will focus on a motivational circuit in the \u003cb\u003ebrain\u003c/b\u003e, centered on the \u003cbr\u003e\namygdala, that underlies human emotion. This neural circuitry of appetitive/\u003cbr\u003e\napproach&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/17015072",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/17015072",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Emotion, motivation, and the brain: reflex foundations in animal and human research.",
+      "citation_authors": "Lang PJ; Davis M",
+      "citation_date": "2006",
+      "citation_publisher": "Prog Brain Res",
+      "citation_journal_title": "Progress in brain research",
+      "citation_pmid": "17015072",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/17015072",
+      "citation_volume": "156",
+      "citation_firstpage": "3",
+      "citation_doi": "10.1016/S0079-6123(06)56001-7",
+      "citation_issn": "0079-6123"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Brain structures in pediatric maltreatment-related posttraumatic ...",
+   "htmlTitle": "\u003cb\u003eBrain\u003c/b\u003e structures in pediatric maltreatment-related posttraumatic ...",
+   "link": "https://www.ncbi.nlm.nih.gov/pubmed/12460690",
+   "displayLink": "www.ncbi.nlm.nih.gov",
+   "snippet": "Biol Psychiatry. 2002 Dec 1;52(11):1066-78. Brain structures in pediatric \nmaltreatment-related posttraumatic stress disorder: a sociodemographically \nmatched ...",
+   "htmlSnippet": "Biol Psychiatry. 2002 Dec 1;52(11):1066-78. \u003cb\u003eBrain\u003c/b\u003e structures in pediatric \u003cbr\u003e\nmaltreatment-related posttraumatic stress disorder: a sociodemographically \u003cbr\u003e\nmatched&nbsp;...",
+   "formattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/12460690",
+   "htmlFormattedUrl": "https://www.ncbi.nlm.nih.gov/pubmed/12460690",
+   "pagemap": {
+    "metatags": [
+     {
+      "citation_title": "Brain structures in pediatric maltreatment-related posttraumatic stress disorder: a sociodemographically matched study.",
+      "citation_authors": "De Bellis MD; Keshavan MS; Shifflett H; Iyengar S; Beers SR; Hall J; Moritz G",
+      "citation_date": "12/1/2002",
+      "citation_publisher": "Biol Psychiatry",
+      "citation_journal_title": "Biological psychiatry",
+      "citation_pmid": "12460690",
+      "citation_abstract_html_url": "https://www.ncbi.nlm.nih.gov/pubmed/12460690",
+      "citation_volume": "52",
+      "citation_issue": "11",
+      "citation_firstpage": "1066",
+      "citation_doi": "10.1016/s0006-3223(02)01459-2",
+      "citation_issn": "0006-3223"
+     }
+    ]
+   }
+  }
+ ]
+}
+
+export default sample
